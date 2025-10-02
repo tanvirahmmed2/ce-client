fa4a46e1368e69e0c4c1fb15ade99b2053cad845
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-03 02:21:42 +0600

    update

--- src/pages/Research.jsx
+++ src/pages/Research.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Research = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="w-full min-h-[800px] p-6 flex flex-col items-center justify-center gap-8">
+      <h1 className='text-4xl font-bold text-emerald-600'>Research Field</h1>
+      <p className='text-base sm:text-lg text-center'>Our multidisciplinary approach spans various fields of climate science and environmental research</p>
+     
+
+    </section>
   )
 }
 
