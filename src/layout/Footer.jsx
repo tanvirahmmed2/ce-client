1b02fde58c20142234923f1940a3760b978401b9
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-02 11:37:17 +0600

    update

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+import { RiGlobalLine } from "react-icons/ri";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full flex flex-col items-center justify-center bg-gray-600 text-white/70'>
+      <footer className='w-full flex flex-col md:flex-row px-6 py-7 items-center justify-center gap-4'>
+        <div className='w-full flex flex-col items-start justify-center gap-4'>
+          <p className='text-green-600 text-4xl'><RiGlobalLine /></p>
+          <div className='w-auto  flex flex-col items-start justify-center'>
+            <h1 className='font-semibold text-white'>CCIRL</h1>
+            <p className='text-xs'>Climate Change and Impact Resonance Lab</p>
+          </div>
+        </div>
+        <div className='w-full flex flex-col items-start justify-center gap-4'>
+          <h1 className='font-semibold text-white'>Quick links</h1>
+          <div className='flex flex-col'>
+            <Link>Mission</Link>
+            <Link>Research</Link>
+            <Link>News</Link>
+            <Link>Events</Link>
+          </div>
+        </div>
+        <div className='w-full flex flex-col items-start justify-center gap-4'>
+          <h1 className='font-semibold text-white'>Resources</h1>
+          <div className='flex flex-col'>
+            <Link>Publications</Link>
+            <Link>Data Portal</Link>
+            <Link>Tools</Link>
+            <Link>Library</Link>
+          </div>
+        </div>
+
+        <div className='w-full flex flex-col items-start justify-center gap-4'>
+          <h1 className='font-semibold text-white'>Connect</h1>
+          <div className='flex flex-col'>
+            <a href="mailto:info@ccirl.edu">info@ccirl.edu</a>
+            <a href="tel:+1 (555) 123-4567">+1 (555) 123-4567</a>
+            <p>Twitter</p>
+            <p>Linkedin</p>
+          </div>
+        </div>
+
+      </footer>
+      <div className='w-full flex items-center justify-center px-5 py-7 text-center border-t-2'>
+        <p className='text-xs sm:text-base'>© 2025 Climate Change and Impact Resonance Lab. All rights reserved.</p>
+
+      </div>
+    </section>
   )
 }
 
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
   const { sidebar, setSidebar } = useContext(ThemeContext)
   console.log(sidebar)
   return (
-    <div className={`w-[300px] min-h-screen fixed bg-gray-600 z-50 right-0 flex md:hidden ${sidebar ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out flex-col items-end justify-start gap-4 text-white px-6 py-6`}>
+    <div className={`w-[250px] min-h-screen fixed bg-gray-600 z-50 right-0 flex md:hidden ${sidebar ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out flex-col items-end justify-start gap-4 text-white px-6 py-6`}>
       <a href="/" className='w-auto  flex flex-col items-end justify-center gap-2'>
 
         <p className='text-green-600 text-4xl'><RiGlobalLine /></p>
@@ -28,7 +28,7 @@ const Sidebar = () => {
         <Link to='/gallery' className='w-auto  flex justify-center items-center px-2 hover:px-6 border-green-600'>Gallery</Link>
         <Link to='/council' className='w-auto  flex justify-center items-center px-2 hover:px-6 border-green-600'>Council</Link>
         <Link to='/contact' className='w-auto  flex justify-center items-center px-2 hover:px-6 border-green-600'>Contact</Link>
-        <Link to='/registration' className='w-auto  flex justify-center items-center px-2 hover:px-6 border-green-600'>Join</Link>
+        <Link to='/registration' className='w-auto  flex justify-center items-center px-2 hover:px-6 border-green-600'>Join to Community</Link>
       </div>
       <button onClick={() => setSidebar(!sidebar)} className='w-auto  flex  justify-center items-center px-2  text-2xl mt-10'><FaArrowRightLong /></button>
     </div>
