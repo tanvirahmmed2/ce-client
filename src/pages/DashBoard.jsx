3905104ca4ea1159d26ad5fe089cff3ed73f4de5
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-03 17:41:53 +0600

    update

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const DashBoard = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full flex flex-col md:flex-row '>
+      <div className='w-full md:w-auto h-auto md:min-h-[800px] p-4 flex flex-row md:flex-col items-start justify-start bg-slate-100'>
+        <p>Profile</p>
+        <p>Information</p>
+        <p>Events</p>
+        <p>News</p>
+        <p>Team</p>
+        <p>Gallery</p>
+        <p>Members</p>
+        <p>User</p>
+        <p>Policy</p>
+        <p>Setting</p>
+
+      </div>
+
+    </section>
   )
 }
 
