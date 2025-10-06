d32ccde1d4a14a0cf950f44e9c60a86233db9b98
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-06 12:41:24 +0600

    update

--- src/user/Register.jsx
+++ src/user/Register.jsx
@@ -78,15 +78,15 @@ const Register = () => {
   }
   return (
     <section className='w-full flex items-center justify-center p-6'>
-      <div className='w-full lg:w-3/4 h-auto flex flex-col  items-center justify-center rounded-lg overflow-hidden'>
-        <div className='w-full text-center py-16 flex flex-col items-center justify-center gap-2 bg-gradient-to-br from-emerald-600 to-cyan-700 text-white'>
+      <div className='w-full lg:w-3/4 h-auto flex flex-col lg:flex-row items-center justify-center rounded-lg overflow-hidden shadow-lg bg-gradient-to-br from-emerald-600 to-cyan-700 text-white'>
+        <div className='w-full text-center py-16 flex flex-col items-center justify-center gap-2 '>
           <h1>Welcom to</h1>
           <h1 className='text-xl font-semibold'>Climate Change and Impact Resonance Lab</h1>
           <p>Be a proud member for the lab and contribute for the world</p>
           <Link to='/login' className='text-xs mt-6 italic'>Already registered?</Link>
         </div>
-        <div className='w-full h-auto flex flex-col p-4 py-8 items-center justify-center bg-gray-200'>
-          <form onSubmit={HandleSubmit} className='w-full flex flex-col gap-4 items-center justify-center'>
+        <div className='w-full h-auto flex flex-col p-4 py-8 items-center justify-center bg-gray-200 text-black'>
+          <form onSubmit={HandleSubmit} className='w-full flex flex-col gap-4 items-center justify-center '>
 
 
             <div className='w-full flex flex-col items-start justify-start gap-2'>
